b04fc6794f7895ec59554d2b4b16dd7f4523dc4d
Eguono <efeguono@gmail.com>
2017-02-16 21:56:50 +0100

    Data Base Model

---
+++ model/room.js
@@ -0,0 +1,22 @@
+var mongoose = require(mongoose);
+
+var roomSchema = mongoose.Schema({
+    name:{type:String},
+    available:{type:String},
+    number:{type:Number},
+});
+
+
+module.exports = mongoose.model('room', roomSchema);
+
+var rooms = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
