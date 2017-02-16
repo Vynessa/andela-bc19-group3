527852a165c675d8d6c6b82d2c43b88ac1d3cd1a
Eguono <efeguono@gmail.com>
2017-02-16 14:35:49 +0100

    Set Up of Express Web Server

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+var port = 3000;
+
+app.use(express.static(process.cwd() + './public'));
+app.use(expres.static(process.cwd() + './src'));
+
+app.get('/', function (req, res) {
+    res.send('Hello Vanessa!!!!');
+});
+
+app.get('/rooms', function (req, res) {
+    res.send('rooms');
+});
+
+app.get('/about', function(req,res){
+    res.send('About Group members');
+});
+
+app.listen(port, function () {
+    console.log('Listen on port: ' + port);
+});
