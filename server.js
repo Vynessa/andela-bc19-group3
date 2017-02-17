f0b144e03f2a14ee0d06e5c0501207491c6d4725
vynessa <vanessa.ejikeme@gmail.com>
2017-02-17 18:11:47 +0100

    Route pages to the local server

--- server.js
+++ server.js
@@ -10,34 +10,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html");
 })
 
-/*app.get('/rooms', function(req, res){
-  res.sendFile(__dirname + '/rooms.html');
-});
-
-app.get('/about', function(req, res){
-  res.sendFile(__dirname + '/about.html');
-});
-
-app.get('/meetingroom', function(req, res){
-	res.sendFile(__dirname + '/meetingroom.html')
-})
-
-app.get('/gameroom', function(req, res){
-	res.sendFile(__dirname + '/gameroom.html')
-})
-
-app.get('/quiettimeroom', function(req, res){
-	res.sendFile(__dirname + '/quiettimeroom.html')
-})
-
-app.get('/learningroom', function(req, res){
-	res.sendFile(__dirname + '/learningroom.html')
-})
-
-app.get('/workingroom', function(req, res){
-	res.sendFile(__dirname + '/workingroom.html')
-})*/
-
 app.listen(port, function(){
 console.log('Server running on port ' + port);
 });
